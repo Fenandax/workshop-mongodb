c46189a2d877b048cd24a07610126baf881681e6
Fernanda de Azevedo Fiuza <fernandafiuza@outlook.com.br>
2022-05-03 09:55:52 -0300

    entidade user

--- workshopmongo/src/main/java/com/example/demo/domain/User.java
+++ workshopmongo/src/main/java/com/example/demo/domain/User.java
@@ -1,9 +1,11 @@
 package com.example.demo.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class User {
-
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
 	private String id;
 	private String name;
 	private String email;
