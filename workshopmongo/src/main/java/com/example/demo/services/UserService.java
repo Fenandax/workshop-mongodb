7f4cadfadef0ecf5bbc6f405e03d54ee1822cd3d
Fernanda de Azevedo Fiuza <fernandafiuza@outlook.com.br>
2022-05-03 11:20:43 -0300

    atualização com PUT

--- workshopmongo/src/main/java/com/example/demo/services/UserService.java
+++ workshopmongo/src/main/java/com/example/demo/services/UserService.java
@@ -13,30 +13,40 @@ import com.example.demo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
-	
+
+	public User update(User entity) {
+		User newObj = repo.findById(entity.getId()).get();
+		updateData(newObj, entity);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
-
